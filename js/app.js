a774c400b85576c3f2fbf210305ae1885dff500a
luantaraschi <luantaraschi@gmail.com>
2024-10-10 16:03:43 -0300

    botao reiniciar agora funciona

--- js/app.js
+++ js/app.js
@@ -36,3 +36,9 @@ function embaralha(lista) {
             [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
+
+function reiniciar() {
+    amigos =  [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
